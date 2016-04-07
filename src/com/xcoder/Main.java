bbeab36a49b64f2145a601d6b13f8aad22977a48
xcoder <xcoder@sina.com>
2016-04-07 15:11:46 +0800

    xcoder :add README file

--- src/com/xcoder/Main.java
+++ src/com/xcoder/Main.java
@@ -1,8 +1,16 @@
+/**
+ * Copyright : xcoder boolyahoo@gmail.com
+ *
+ * */
+
 package com.xcoder;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("sddddddddddddd");
+
     }
+
+
 }
