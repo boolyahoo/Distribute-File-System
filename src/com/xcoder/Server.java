4b57d4b0b06fc11bd084189337ebeca69c885ee3
boolyahoo <boolyahoo@gmail.com>
2016-06-03 16:22:35 +0800

    尝试以Server形式启动slave

--- src/com/xcoder/Server.java
+++ src/com/xcoder/Server.java
@@ -21,23 +21,21 @@ import org.apache.commons.cli.*;
 public class Server {
     private static PrintStream out = System.out;
     // port for master node
-    public static final int port = 8080;
+    public static int port = 8080;
     public static final String host = "localhost";
     public static boolean isMaster = true;
 
 
     public void run(String args[]) throws Exception {
-        parseArgs(args);
-        if (isMaster) {
-            initAsMaster();
-        }else{
-            initAsSlave();
+        if (parseArgs(args)) {
+            initServer();
         }
+
     }
 
 
-    private void initAsMaster() throws Exception {
-        out.println("current node : master");
+    private void initServer() throws Exception {
+        out.println("current node : " + (isMaster ? "master" : "slave"));
         out.println("port:" + port);
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
@@ -45,9 +43,9 @@ public class Server {
         try {
             sBoot.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
-                    .childHandler(new ServerInitializer())
                     .option(ChannelOption.SO_BACKLOG, 128)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(isMaster ? new ServerInitializer() : new SlaveInitializer());
             out.println("Server started");
             ChannelFuture cFuture = sBoot.bind(port).sync();
             cFuture.channel().closeFuture().sync();
@@ -59,33 +57,13 @@ public class Server {
     }
 
 
-    private void initAsSlave(){
-        out.println("current node : slave");
-        EventLoopGroup group = new NioEventLoopGroup();
-        try {
-            Bootstrap bStrap = new Bootstrap()
-                    .group(group)
-                    .channel(NioSocketChannel.class)
-                    .handler(new SlaveInitializer());
-            bStrap.connect(host, port).sync().channel();
-            out.println("slave started!");
-            while(true){
-                // 添加空转，防止slave退出
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            group.shutdownGracefully();
-        }
-    }
-
-
     /**
      * parse args for current process
      *
      * @param args : 命令行
+     * @return : true if args are all right
      */
-    private void parseArgs(String args[]) {
+    private boolean parseArgs(String args[]) {
         /**
          * command line
          * -m : set current server runs as a master node
@@ -96,6 +74,7 @@ public class Server {
         opts.addOption("h", false, "help info");
         opts.addOption("m", false, "set current process run as a master node");
         opts.addOption("s", false, "set current process run as a slave node");
+        opts.addOption("p", false, "current server port");
         BasicParser parser = new BasicParser();
         CommandLine cl;
         try {
@@ -105,18 +84,33 @@ public class Server {
                     HelpFormatter hf = new HelpFormatter();
                     hf.printHelp("options", opts);
                 } else {
+                    if (cl.hasOption("p")) {
+                        port = Integer.parseInt(cl.getOptionValue("p"));
+                    } else {
+                        HelpFormatter hf = new HelpFormatter();
+                        hf.printHelp("options", opts);
+                        return false;
+                    }
                     if (cl.hasOption("m")) {
                         isMaster = true;
                     } else if (cl.hasOption("s")) {
                         isMaster = false;
+                    } else {
+                        HelpFormatter hf = new HelpFormatter();
+                        hf.printHelp("options", opts);
+                        return false;
                     }
                 }
             } else {
                 out.println("command line error");
+                HelpFormatter hf = new HelpFormatter();
+                hf.printHelp("options", opts);
+                return false;
             }
         } catch (ParseException e) {
             e.printStackTrace();
         }
+        return true;
     }
 
 
